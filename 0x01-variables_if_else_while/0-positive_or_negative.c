027b82bba89fb36628cdbc3cbcc1e2459afd0ba2
epsroot <mwangiperis60@gmail.com>
2022-07-01 19:06:50 -0700

    Pos neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,16 @@
+#include <stdio.h>
+
 #include <stdlib.h>
 
 #include <time.h>
 
-#include <stdio.h>
-
 /**
 
- * main - Entry point
+ * main - prints the logic format for positve or negative numbers
 
- *
+ * Return: Always 0 (success)
 
- * Return: Always 0 (Success)
-
- */
+ **/
 
 
 
@@ -22,35 +20,27 @@ int main(void)
   
   int n;
   
-
-  
   srand(time(0));
   
   n = rand() - RAND_MAX / 2;
   
+
+  
   if (n > 0)
     
-    {
-      
-      printf("%d is positive\n", n);
-      
-    }
+    printf("%d is positive\n", n);
   
-  else if (n < 0)
+
+  
+  else if (n == 0)
     
-    {
-      
-      printf("%d is negative\n", n);
-      
-    }
+    printf("%d is zero\n", n);
+  
+
   
   else
     
-    {
-      
-      printf("%d is zero\n", n);
-      
-    }
+    printf("%d is negative\n", n);
   
   return (0);
   
