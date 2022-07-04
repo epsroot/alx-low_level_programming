dcf4aafd7d27908eea80d0fdd4e6906ebb6aec45
epsroot <mwangiperis60@gmail.com>
2022-07-04 16:07:39 -0700

    n +|-

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,47 +1,24 @@
 #include <stdio.h>
-
-#include <stdlib.h>
-
+#include <stdlid.h>
 #include <time.h>
-
 /**
-
- * main - prints the logic format for positve or negative numbers
-
- * Return: Always 0 (success)
-
- **/
-
-
-
+ *main - assigns a random number to var n each time its executed
+ *also prints the last digit of the no stored in var n
+ *Return : Often 0 (Success)
+ */
 int main(void)
-  
 {
-  
-  int n;
-  
-  srand(time(0));
-  
-  n = rand() - RAND_MAX / 2;
-  
+int n;
 
-  
-  if (n > 0)
-    
-    printf("%d is positive\n", n);
-  
-
-  
-  else if (n == 0)
-    
-    printf("%d is zero\n", n);
-  
-
-  
-  else
-    
-    printf("%d is negative\n", n);
-  
-  return (0);
-  
+srand(time(0));
+n = rand(void) - RAND_MAX / 2;
+if (n == 0)
+	printf("%d is zero\n", n);
+if (n > 0)
+	printf("%d is positive\n", n);
+if (n < 0)
+{
+	printf("%d is negative\n", n);
+}
+return (0);
 }
