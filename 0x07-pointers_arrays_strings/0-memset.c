62659a99816a3937710cc83b68752418c26cd0c9
epsroot <mwangiperis60@gmail.com>
2022-07-21 14:04:12 -0700

    Locating chars

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+
+/**
+ *_memset - this is a memory set function
+ *@s: string
+ *@b:  a character
+ *@n: an integer
+ *Return: a string
+ */
+
+
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+	unsigned int k;
+
+
+
+	for (k = 0; k < n; k++)
+
+		s[k] = b;
+
+
+
+	return (s);
+
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pretty much about memory (pointer)
